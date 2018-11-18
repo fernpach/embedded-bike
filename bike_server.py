b7b9f21c980e08ca4fa6571a9780b54024c627ad
Fernando Pacheco <fpacheco0707@yahoo.com>
2018-11-17 18:31:48 -0800

    Add basic sql connection

--- bike_server.py
+++ bike_server.py
@@ -1,9 +1,31 @@
-import mraa
+#import mraa
+import pymysql
+
+
+def connect():
+    user = ''
+    passwd = ''
+    db = ''
+
+    return pymysql.connect(host='127.0.0.1',
+                           user=user,
+                           password=passwd,
+                           db=db) 
 
 
 def main():
     """Script entry point."""
-    pass
+    try:
+        connection = connect()
+
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM entries"
+            cursor.execute(sql)
+            result = cursor.fetchone()
+            print(result)
+    finally:
+        connection.close() 
+
 
 if __name__ == '__main__':
     main()

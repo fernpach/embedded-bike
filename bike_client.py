3f55f08d6b762352ba231d6aadd151f60a1dff78
Fernando Pacheco <fpacheco0707@yahoo.com>
2018-11-17 18:57:48 -0800

    Add sample metric stub functionality

--- bike_client.py
+++ bike_client.py
@@ -3,10 +3,13 @@ bike_client
 """
 # Uncomment when ready to test on board
 #import mraa
+import random
+from datetime import datetime
 
 
 METRIC_KEYS = ['workout_id', 'speed', 'distance',
                'calories_burned', 'heart_rate', 'timestamp']
+
 
 def sample():
     raw_data = _sample()
@@ -17,4 +20,16 @@ def sample():
 def _sample():
     # This will read GPIO pins (eventually)
     # for testing let's just manufacture data
+    wid = generate_workout_id()       # unique workout id 
+    speed = random.randint(1, 20)     # miles/hr
+    distance = random.randint(1, 10)  # miles
+    calories_burned = random.randint(1, 300)
+    heart_rate = random.randint(1, 120)
+    timestamp = datetime.now()
+
+    return [wid, speed, distance, calories_burned,
+            heart_rate, timestamp]
+
+
+def generate_workout_id():
     
